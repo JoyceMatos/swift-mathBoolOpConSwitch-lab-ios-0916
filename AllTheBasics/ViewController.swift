a90dc49c94894962427aea9b8bff1337b9257c59
JoyceMatos <j.matosjoyce@gmail.com>
2016-08-30 18:03:15 -0400

    Completed

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -12,12 +12,38 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
     }
     
-    
-   // Implement your functions here
-
-    
-
-
+    func averageIsAbove75(a: Double, b: Double, c: Double) -> Bool {
+        let average = a + b + c
+        switch average / 3 > 75 {
+        case true:
+            return true
+            default: return false
+        }
 }
+    
+    func passwordCombo(username: String, password: Int) -> String {
+        switch username {
+            case "Jerry":
+            return "Welcome"
+            case "Elaine":
+            return "Welcome"
+            case "Michael":
+            return "Welcome"
+        default: return "Access Denied"
+        }
+    }
+    
+    func numberGenerator(a: String, b: Float) -> Float {
+   let theseNumbers = (a, b)
+        switch theseNumbers {
+        case("1"..."5", 10.5...15.0):
+            return Float(a)! * b
+        default: return 0.0
+        }
+        
+    }
+}
+
